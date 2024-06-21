6b839111b6198394a261203e02d7caa865196988
Adarsh Pandey <adarsh@archlinux.localdomain>
2024-06-21 13:37:04 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+import Layout from '../(auth)/layout'
+import SignInPage from '../(auth)/sign-in/[[...sign-in]]/page'
+
+const Home = () => {
+  return (
+    <div>
+      {/* <UserButton afterSignOutUrl='/sign-in'/> */}
+    </div>
+  )
+}
+
+export default Home
